ed0e2820e9b53a4412417301975de6db5d9af74e
borsutzha <borsutzky@hansaluftbild.de>
2017-04-07 17:52:03 +0200

    Added AutocompleteController Class

---
+++ src/main/java/com/pimpelkram/universalfilefinder/AutocompleteController.java
@@ -0,0 +1,10 @@
+package com.pimpelkram.universalfilefinder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AutocompleteController {
+
+	@FXML
+	TextField autocomplete;
+}
