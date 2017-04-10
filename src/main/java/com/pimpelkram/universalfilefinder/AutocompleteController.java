91732ad7d7c44d0c1ab4ce544dfa2ede51cad65a
borsutzha <borsutzky@hansaluftbild.de>
2017-04-10 15:20:16 +0200

    Neu: FileWalkingTask (Basic Liste mit allen Packages), Controller startet den Task

--- src/main/java/com/pimpelkram/universalfilefinder/AutocompleteController.java
+++ src/main/java/com/pimpelkram/universalfilefinder/AutocompleteController.java
@@ -1,10 +1,36 @@
 package com.pimpelkram.universalfilefinder;
 
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Inject;
+import com.pimpelkram.universalfilefinder.config.Settings;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class AutocompleteController {
 
+	Logger logger = LoggerFactory.getLogger(AutocompleteController.class);
+
+	@Inject
+	Settings settings;
+
+	@Inject
+	FileWalkingTask fwt;
+
 	@FXML
 	TextField autocomplete;
+
+	public void initialize() {
+		logger.debug("Start init AutocompleteController.");
+		final Thread fwtThread = new Thread(fwt);
+		fwtThread.setDaemon(true);
+		fwtThread.start();
+		final AutoCompletionBinding<String> acb = TextFields.bindAutoCompletion(autocomplete, fwt.getPackageList());
+		logger.debug("Ende init AutocompleteController.");
+
+	}
 }
---
+++ src/main/java/com/pimpelkram/universalfilefinder/FileWalkingTask.java
@@ -0,0 +1,55 @@
+package com.pimpelkram.universalfilefinder;
+
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Inject;
+import com.pimpelkram.universalfilefinder.config.Settings;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+
+public class FileWalkingTask extends Task<ObservableList<String>> {
+
+	private final Logger logger = LoggerFactory.getLogger(FileWalkingTask.class);
+	private final ObservableList<String> packageList = FXCollections.observableArrayList();
+
+	@Inject
+	Settings settings;
+
+	@Override
+	protected ObservableList<String> call() throws Exception {
+		logger.debug("FileWalkerTask call() start...");
+		for (final String rootPathString : settings.getRootFolderList()) {
+			try {
+				logger.debug("rootPathString : " + rootPathString);
+				logger.debug(String.valueOf(Files.exists(Paths.get(rootPathString))));
+				Files.walk(Paths.get(rootPathString)).filter(p -> !Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS))
+						// .filter(p -> p.toString().matches(getPositiveRegex())
+						// && !p.toString().matches(getNegativeRegex()))
+						.forEach(p -> handlePathFile(p));
+				logger.debug("finished for loop");
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		}
+		logger.debug("FileWalkerTask call() end...");
+		return getPackageList();
+	}
+
+	public ObservableList<String> getPackageList() {
+		return packageList;
+	}
+
+	private void handlePathFile(Path p) {
+		logger.debug(p.toString());
+		getPackageList().add(p.toString());
+	}
+
+}
