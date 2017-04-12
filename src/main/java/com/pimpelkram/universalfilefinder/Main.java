b681d37f68179d95d457d5e29cbd2185541f3597
borsutzha <borsutzky@hansaluftbild.de>
2017-04-12 16:48:31 +0200

    added icons

--- src/main/java/com/pimpelkram/universalfilefinder/Main.java
+++ src/main/java/com/pimpelkram/universalfilefinder/Main.java
@@ -13,6 +13,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -41,6 +42,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/search.png")));
 		logger.debug("Main FXApplication 'start' start.");
 		// primaryStage.initStyle(StageStyle.TRANSPARENT);
 		primaryStage.setAlwaysOnTop(true);
