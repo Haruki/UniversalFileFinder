d4cda7431d175651084ef0371e2d778f1cb698c0
borsutzha <borsutzky@hansaluftbild.de>
2017-04-26 16:31:08 +0200

    completed transparent effects (icon)

--- src/main/java/com/pimpelkram/universalfilefinder/Main.java
+++ src/main/java/com/pimpelkram/universalfilefinder/Main.java
@@ -16,6 +16,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -34,6 +38,8 @@ public class Main extends Application {
 	@Override
 	public void init() {
 		logger.debug("Main FXApplication 'init' start.");
+		// logger.debug("init SvgImageLoader Factory...");
+		// SvgImageLoaderFactory.install();
 		logger.debug("Main FXApplication 'init' end.");
 	}
 
@@ -54,13 +60,23 @@ public class Main extends Application {
 		fxmlLoader.load();
 		final Parent view = fxmlLoader.getRoot();
 		final Node node = view.lookup("#dragButton");
-		node.setStyle("-fx-background-image: url('/search.png');" + "-fx-background-color: #FFFFFF;"
-				+ "-fx-background-size: 20 20; " + "-fx-background-position: center;"
-				+ "-fx-background-repeat: no-repeat;" + "-fx-background: transparent;");
+		final Node image = view.lookup("#image");
+		final Node imageStackPane = view.lookup("#imageStackPane");
+		final Node mainHBox = view.lookup("#mainHBox");
+		((HBox) mainHBox).setBackground(Background.EMPTY);
+		((StackPane) imageStackPane).setBackground(Background.EMPTY);
+		// node.setStyle("-fx-background-image: url('/search.png');" +
+		// "-fx-background-color: #FFFFFF;"
+		// + "-fx-background-size: 20 20; " + "-fx-background-position: center;"
+		// + "-fx-background-repeat: no-repeat;" + "-fx-background:
+		// transparent;");
 		logger.debug("Found DragButton: " + (node != null));
-		EffectUtilities.makeDraggable(primaryStage, node);
+		EffectUtilities.makeDraggable(primaryStage, image);
 		primaryStage.setTitle("UniversalFileFinder");
-		primaryStage.setScene(new Scene(view));
+		final Scene scene = new Scene(view);
+		logger.debug(scene.getAntiAliasing().toString());
+		scene.setFill(Color.TRANSPARENT);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 		logger.debug("Main FXApplication 'start' end.");
 	}
