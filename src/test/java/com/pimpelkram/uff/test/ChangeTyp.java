1e1b867b9c0c5d066ae6e18a1da592fac5c9bcf5
Homer <hb@hb.gl>
2017-06-05 18:54:08 +0200

    Erster Test mit RxJava inkl. FileWatcher

---
+++ src/test/java/com/pimpelkram/uff/test/ChangeTyp.java
@@ -0,0 +1,8 @@
+package com.pimpelkram.uff.test;
+
+/**
+ * Created by Homer on 05.06.2017.
+ */
+public enum ChangeTyp {
+    U,D,I;
+}
--- src/test/java/com/pimpelkram/uff/test/MainTest.java
+++ src/test/java/com/pimpelkram/uff/test/MainTest.java
@@ -1,19 +1,37 @@
 package com.pimpelkram.uff.test;
 
+import io.reactivex.Observable;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchService;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 
 /**
  * Created by Homer on 05.06.2017.
  */
 public class MainTest {
-    public static void  main(String[] args) {
-      Config config;
-        Yaml yaml = new Yaml(new Constructor(Config.class));
-        config = (Config) yaml.load(MainTest.class.getResourceAsStream("/testConfig.yml"));
-        System.out.println(config);
-        for (String dir : config.directories) {
-            System.out.println(dir);
-        }
+    public static void main(String[] args) {
+
+        Observable<DirectoryEvent> directoryStream = Observable.create(o -> {
+            Path configPath = Paths.get("/testConfig.yml");
+            WatchService watcher = configPath.getFileSystem().newWatchService();
+            Config config;
+            Yaml yaml = new Yaml();
+            config = yaml.loadAs(MainTest.class.getResourceAsStream(configPath.toString()), Config.class);
+            System.out.println(config);
+            for (String dir : config.directories) {
+                System.out.println(dir);
+                o.onNext(new DirectoryEvent(ChangeTyp.I, dir));
+            }
+            configPath.register(watcher, ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+
+        });
+
+        directoryStream.subscribe(event -> System.out.println(event.getChangeTyp() + "  " + event.getPathName()));
     }
 }
