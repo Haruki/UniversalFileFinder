b2facf800df2ab8f44e6a0b7274d0bfe05a4ffd3
haruki <hb@hb.gl>
2019-05-19 18:42:38 +0200

    filewalking task support for multiple regex exclude filter

--- src/main/java/com/pimpelkram/universalfilefinder/FileWalkingTask.java
+++ src/main/java/com/pimpelkram/universalfilefinder/FileWalkingTask.java
@@ -1,29 +1,23 @@
 package com.pimpelkram.universalfilefinder;
 
-import java.nio.file.FileVisitOption;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.inject.Inject;
 import com.pimpelkram.universalfilefinder.config.Settings;
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
 import javafx.concurrent.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.*;
 
 public class FileWalkingTask extends Task<ObservableMap<String, String>> {
 
-    private final Logger                        logger      = LoggerFactory.getLogger(FileWalkingTask.class);
+    private final Logger logger = LoggerFactory.getLogger(FileWalkingTask.class);
     private final ObservableMap<String, String> packageList = FXCollections.observableHashMap();
 
     @Inject
-    Settings                                    settings;
+    Settings settings;
 
     @Override
     protected ObservableMap<String, String> call() throws Exception {
@@ -35,10 +29,7 @@ public class FileWalkingTask extends Task<ObservableMap<String, String>> {
                 Files.walk(Paths.get(rootPathString), 1, FileVisitOption.FOLLOW_LINKS)
                         .filter(p -> !Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS))
                         .peek(p -> System.out.println("Before Filter!: " + p.toString()))
-                        // .filter(p -> p.toString().matches(getPositiveRegex())
-                        // && !p.toString().matches(getNegativeRegex()))
-                        .filter(p -> this.settings.getExcludeRegex().stream().anyMatch(pattern -> !p.toString().matches(pattern)))
-                        .forEach(p -> this.handlePathFile(p));
+                        .forEach(this::handlePathFile);
                 this.logger.debug("finished for loop");
             } catch (final Exception e) {
                 e.printStackTrace();
@@ -48,7 +39,7 @@ public class FileWalkingTask extends Task<ObservableMap<String, String>> {
         return this.getPackageList();
     }
 
-    public ObservableMap<String, String> getPackageList() {
+    ObservableMap<String, String> getPackageList() {
         return this.packageList;
     }
 
@@ -57,7 +48,14 @@ public class FileWalkingTask extends Task<ObservableMap<String, String>> {
         final String[] splits = p.toString().split("\\\\");
         final int lowIndex = splits.length - 3;
         final int useIndex = lowIndex < 0 ? 0 : lowIndex;
-
+        //filter global regex:
+        if (this.settings.getExcludeRegex() != null) {
+            for (String rx : this.settings.getExcludeRegex()) {
+                if (p.toString().matches(rx)) {
+                    return;
+                }
+            }
+        }
         // logger.debug("Splitlength: " + splits.length);
         final String shortString = "(" + splits[useIndex] + ") " + splits[splits.length - 1];
         // logger.debug("handlePathFile shortString: " + shortString);
